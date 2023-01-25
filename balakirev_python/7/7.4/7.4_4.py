0b99af045e8e65ad3e2d2c24186aac919c09fb33
Rustam Batyrshin <truezorvis@localhost>
2023-01-25 14:37:33 +0500

    Added 7.4

---
+++ balakirev_python/7/7.4/7.4_4.py
@@ -0,0 +1,16 @@
+t = {'ё': 'yo', 'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ж': 'zh',
+     'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p',
+     'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh',
+     'щ': 'shch', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'}
+
+
+def translate_to_latin(string, sep='-'):
+    output = ''
+    for char in string.lower():
+        output += t.get(char, char)
+
+    return output.replace(' ', sep)
+
+
+print(translate_to_latin('Лучший курс по Python!'))
+print(translate_to_latin('Лучший курс по Python!', sep='.'))
